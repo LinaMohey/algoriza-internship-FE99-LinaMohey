d5d7122dc9bd29aec2605f4fe271dab96822b1ef
Lina Mohey <linamohey15@outlook.com>
2023-11-28 18:34:21 -0800

    edit welcome-card

--- src/views/welcome-card.vue
+++ src/views/welcome-card.vue
@@ -1,9 +1,13 @@
 <template>
   <div class="welcome-card">
+    <div class="welcome-image">
+      <img src="@/assets/welcome-section/welcome.png" alt="welcome-image" />
+    </div>
     <h4>Welcome to My Dream Place</h4>
     <p>
       You can now enjoy your travel experience with easy access to resources and
       more
     </p>
+    <router-link :to="{ name: 'search-results' }"> Get Started</router-link>
   </div>
 </template>
